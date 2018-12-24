4bbda1659c11e8734a9274263bde02cbf6391c63
Chris Collins <chriscollins238@gmail.com>
2018-12-24 12:29:12 +0000

    Remove .cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/chriscollins/GitHub/chrisssy.com/src/layouts/index.js"
-  import data from "/Users/chriscollins/GitHub/chrisssy.com/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
