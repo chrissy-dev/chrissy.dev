432a79ab07a23865fd7d2229a7e286ed0b8ee78b
Chris Collins <chriscollins238@gmail.com>
2019-07-12 21:00:47 +0100

    Remove ordinal date filter

--- .eleventy.js
+++ .eleventy.js
@@ -38,11 +38,6 @@ module.exports = function (eleventyConfig) {
     });
   }
 
-  // Ordinal date filter
-  eleventyConfig.addFilter("ordinal", function(date) {
-    return date + (date > 0 ? ['th', 'st', 'nd', 'rd'][(date > 3 && date < 21) || date % 10 > 3 ? 0 : date % 10] : '');
-  });
-
   eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
