3882712077b23650c13a12cdb4947345c3436a58
Chris Collins <chriscollins238@gmail.com>
2019-09-30 10:56:05 +0100

    Add page count to webmention api

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -3,7 +3,7 @@ const token = process.env.WEBMENTIONS_TOKEN;
 const domain = 'www.chriscollins.me';
 
 module.exports = async function webmentions() {
-    return fetch(`https://webmention.io/api/mentions.jf2?domain=${domain}&token=${token}`)
+    return fetch(`https://webmention.io/api/mentions.jf2?domain=${domain}&token=${token}&per-page=200`)
     .then((result) => result.json())
     .then((data) => {            
       return data.children || [];
