a693697317b60fd4004f6bbdad05d9bf6d3d3459
Chris Collins <chriscollins238@gmail.com>
2019-11-03 19:05:02 +0000

    Only optimise photos that need optimised

--- optimisePhotos.js
+++ optimisePhotos.js
@@ -1,6 +1,7 @@
 const Jimp = require('jimp');
 const path = require("path");
 const glob = require('fast-glob');
+const fs = require('fs');
 
 /**
  * Resize + optimize images.
@@ -10,35 +11,44 @@ const glob = require('fast-glob');
  */
 
 const optimise = async (images, outputDir) => {
-  console.log(`Attempting to generate images...`);
+  let toOptimise = [];
 
-  await Promise.all(
-    images.map(async imgPath => {
+  images.map(image => {
+    const fileName = path.basename(image, '.jpg') || path.basename(image, '.JPG');
+
+    if (!fs.existsSync(`${outputDir}/${fileName}_optimised.jpg`)) { 
+      toOptimise.push(image);
+      console.log(`Added ${fileName} to queue`)
+    }
+  })
+
+  await Promise.all(    
+    toOptimise.map(async imgPath => {
       const image = await Jimp.read(imgPath);
       const fileName = path.basename(imgPath, '.jpg') || path.basename(imgPath, '.JPG');
       
-      await image.quality(80) // set JPEG quality
-      await image.writeAsync(`${outputDir}/${fileName}_optimised.jpg`);
-      console.log(`Generated: ${outputDir}/${fileName}_optimised.jpg`);
-
-      await image.resize(1920, Jimp.AUTO) // resize
-      await image.quality(80) // set JPEG quality
-      await image.writeAsync(`${outputDir}/${fileName}_fullhd.jpg`);
-      console.log(`Generated: ${outputDir}/${fileName}_fullhd.jpg`);
-
-      await image.resize(1280, Jimp.AUTO) // resize
-      await image.quality(80) // set JPEG quality
-      await image.writeAsync(`${outputDir}/${fileName}_hd.jpg`);
-      console.log(`Generated: ${outputDir}/${fileName}_hd.jpg`);
-
-      await image.cover(560, 560) // resize
-      await image.quality(80) // set JPEG quality
-      await image.writeAsync(`${outputDir}/${fileName}_560.jpg`);
-      console.log(`Generated: ${outputDir}/${fileName}_560.jpg`);
+        // await image.quality(80) // set JPEG quality
+        // await image.writeAsync(`${outputDir}/${fileName}_optimised.jpg`);
+        // console.log(`Generated: ${outputDir}/${fileName}_optimised.jpg`);
+  
+        await image.resize(1920, Jimp.AUTO) // resize
+        await image.quality(80) // set JPEG quality
+        await image.writeAsync(`${outputDir}/${fileName}_fullhd.jpg`);
+        console.log(`Generated: ${outputDir}/${fileName}_fullhd.jpg`);
+  
+        // await image.resize(1280, Jimp.AUTO) // resize
+        // await image.quality(80) // set JPEG quality
+        // await image.writeAsync(`${outputDir}/${fileName}_hd.jpg`);
+        // console.log(`Generated: ${outputDir}/${fileName}_hd.jpg`);
+  
+        await image.cover(560, 560) // resize
+        await image.quality(80) // set JPEG quality
+        await image.writeAsync(`${outputDir}/${fileName}_560.jpg`);
+        console.log(`Generated: ${outputDir}/${fileName}_560.jpg`);
     })
   );
 };
 
 (async function () {
-  await glob([`src/assets/images/photos/**/*`]).then(files => optimise(files, `src/assets/images`));
+  await glob([`src/assets/images/photos/*.jpg`]).then(files => optimise(files, `src/assets/images/photos/optimised`));
 })();
