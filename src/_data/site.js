1fd58dfcef64e32856ea97ed9279d72dada0b90c
Chris Collins <chriscollins238@gmail.com>
2020-09-30 14:14:45 +0100

    Update Availability

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,6 +3,10 @@ module.exports = {
   "description": "I’m Chris Collins, and this is my personal space on the web. I use it to house things that interest me like hiking, photography and code.",
   "url": "https://webstarter.chriscollins.me",
   "author": "Chris Collins",
+  "availability": {
+    "status": false,
+    "until": "mid-October 2020"
+  },
   "meta_data": {
     "twitter": "@scottishstoater",
     "default_social_image": "/static/default_social_image.jpg"
