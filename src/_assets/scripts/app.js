a195f75f11440f1f2a4bbb262f299e24814f5e05
Chris Collins <chriscollins238@gmail.com>
2020-11-18 10:18:23 +0000

    Check for elements

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -3,20 +3,24 @@ import LazyLoad from "vanilla-lazyload";
 
 const dateSlots = document.querySelectorAll(".post-date");
 
-dateSlots.forEach(function (slot) {
-  if (slot.dataset.date) {
-    slot.innerHTML = `${formatDistanceToNowStrict(new Date(slot.dataset.date))} ago`;
-  }
-});
+if (dateSlots) {
+  dateSlots.forEach(function (slot) {
+    if (slot.dataset.date) {
+      slot.innerHTML = `${formatDistanceToNowStrict(new Date(slot.dataset.date))} ago`;
+    }
+  });
+}
 
 var navButton = document.querySelector('#nav-toggle');
 
-navButton.addEventListener('click', function () {
-  let expanded = this.getAttribute('aria-expanded') === 'true' || false;
-  this.setAttribute('aria-expanded', !expanded);
-  this.querySelector('.caret').classList.toggle('-rotate-180');
-  let menu = this.nextElementSibling;
-  menu.hidden = !menu.hidden;
-});
+if (navButton) {
+  navButton.addEventListener('click', function () {
+    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    this.querySelector('.caret').classList.toggle('-rotate-180');
+    let menu = this.nextElementSibling;
+    menu.hidden = !menu.hidden;
+  });
+}
 
 var lazyLoadInstance = new LazyLoad();
