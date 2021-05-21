a3613b934307d4f5c1dd21c8b62f0d5cb1e955e2
Chris Collins <chriscollins238@gmail.com>
2021-05-21 14:57:09 +0100

    Minfy HTML output

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
-const { DateTime } = require("luxon");
+const {
+	DateTime
+} = require("luxon");
 const markdownIt = require("markdown-it");
 const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("source/static");
@@ -20,6 +23,20 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+
+	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+		if (outputPath && outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+
+		return content;
 	});
 
 	eleventyConfig.addFilter("cssmin", function (code) {
